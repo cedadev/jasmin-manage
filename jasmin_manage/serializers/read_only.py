df6729b9ba1593856c4d206308722ec1841f6707
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-10-29 17:38:50 +0000

    Add new file missing from previous commit

---
+++ jasmin_manage/serializers/read_only.py
@@ -0,0 +1,19 @@
+class ReadOnlySerializerMixin:
+    """
+    Mixin for serializers that makes them read-only.
+    """
+    def get_fields(self):
+        fields = super().get_fields()
+        for field in fields.values():
+            field.read_only = True
+        return fields
+
+    def save(self, **kwargs):
+        raise RuntimeError('Save is not permitted for read-only serializers.')
+
+
+def read_only_serializer(serializer_class):
+    """
+    Returns a new serializer with the same fields as the given serializer but all read-only.
+    """
+    return type(f'ReadOnly{serializer_class.__name__}', (ReadOnlySerializerMixin, serializer_class), {})
