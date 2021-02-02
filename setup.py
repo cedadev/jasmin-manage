6e8fc32832cd54a5f6c693b3ef9b23fd36ff45d3
Matt Pryor <matt.pryor@stfc.ac.uk>
2021-02-02 18:14:03 +0000

    Move to setup.cfg and pyproject.toml

--- setup.py
+++ setup.py
@@ -1,47 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-import os
-from setuptools import setup, find_packages
-
-
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md')) as f:
-    README = f.read()
+import setuptools
 
 if __name__ == "__main__":
-    setup(
-        name = 'jasmin-manage',
-        setup_requires = ['setuptools_scm'],
-        use_scm_version = True,
-        description = 'API for management of resources on JASMIN.',
-        long_description = README,
-        classifiers = [
-            "Programming Language :: Python",
-            "Framework :: Django",
-            "Topic :: Internet :: WWW/HTTP",
-            "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-        author = 'Matt Pryor',
-        author_email = 'matt.pryor@stfc.ac.uk',
-        url = 'https://github.com/cedadev/jasmin-manage',
-        keywords = 'web django jasmin',
-        packages = find_packages(),
-        include_package_data = True,
-        zip_safe = False,
-        install_requires = [
-            'python-dateutil',
-            'django',
-            'django-markupfield',
-            'markdown',
-            'django-admin-list-filter-dropdown',
-            'django-admin-rangefilter',
-            'django-debug-toolbar',
-            'djangorestframework',
-            'drf-spectacular',
-            'drf-nested-routers',
-        ],
-        extras_require = {
-            'tsunami': ['django-tsunami'],
-            'jasmin': ['jasmin-auth-django-admin'],
-        }
-    )
+    setuptools.setup()
