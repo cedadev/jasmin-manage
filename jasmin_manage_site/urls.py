8b93f1f530b7e62533762e5cc5c39e926542f93b
Matt Pryor <matt.pryor@stfc.ac.uk>
2021-02-15 12:37:32 +0000

    Changes to add authentication views for regular users

--- jasmin_manage_site/urls.py
+++ jasmin_manage_site/urls.py
@@ -6,10 +6,15 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.urls import urlpatterns as authurls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('jasmin_manage.urls')),
+    # In order for the browsable API to pick them up, auth view must be
+    # in the rest_framework namespace
+    path('auth/', include((authurls, 'rest_framework'))),
 ]
 
 if settings.DEBUG:
