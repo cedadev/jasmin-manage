b07b97820a0212dbdc0cb50d496006fefd23fba3
Matt Pryor <matt.pryor@stfc.ac.uk>
2021-03-17 16:01:38 +0000

    Error views that return JSON

--- jasmin_manage_site/urls.py
+++ jasmin_manage_site/urls.py
@@ -4,7 +4,48 @@ jasmin_manage_site URL Configuration
 
 from django.conf import settings
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+from django.views import defaults as error_views
+
+from rest_framework import status
+
+
+# We want to use errors handlers that return HTML for a browser but JSON when asked for
+def page_not_found(request, exception):
+    if request.META.get('HTTP_ACCEPT') == 'application/json':
+        return JsonResponse(
+            dict(detail = 'Not found.'),
+            status = status.HTTP_404_NOT_FOUND
+        )
+    else:
+        return error_views.page_not_found(request, exception)
+
+handler404 = page_not_found
+
+
+def server_error(request):
+    if request.META.get('HTTP_ACCEPT') == 'application/json':
+        return JsonResponse(
+            dict(detail = 'Internal server error.'),
+            status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    else:
+        return error_views.server_error(request)
+
+handler500 = server_error
+
+
+def bad_request(request, exception):
+    if request.META.get('HTTP_ACCEPT') == 'application/json':
+        return JsonResponse(
+            dict(detail = 'Bad request.'),
+            status = status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    else:
+        return error_views.bad_request(request, exception)
+
+handler400 = bad_request
 
 
 # Use the configured authentication URLs, falling back to the rest_framework
