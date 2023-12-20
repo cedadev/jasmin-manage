066dc510f91f73aed8c8689d974063854fe4e27a
nf679 <nicola.farmer@stfc.ac.uk>
2023-12-20 11:38:16 +0000

    Enforce lowercase, hyphens and underscores only

--- jasmin_manage/models/tag.py
+++ jasmin_manage/models/tag.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 
 
@@ -10,8 +11,15 @@ class TagManager(models.Manager):
         return self.get(name=name)
 
 
+class TagField(models.SlugField):
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        return value if value is None else value.lower()
+
+
 class Tag(models.Model):
-    name = models.CharField(max_length=255, null=True)
+    name = TagField(max_length=255, null=True, unique=True)
+    help_text = "Lowercase letters, numbers, underscores or hypens only please."
 
     def natural_key(self):
         return (self.name,)
