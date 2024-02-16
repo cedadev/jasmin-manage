b301b6e1aef54101ae32a648c7ff3b490986676a
Matthew Jones <matthew.j.jones@stfc.ac.uk>
2024-02-16 11:15:08 +0000

    Remove markdown sections of notification to test slcak error

--- jasmin_manage/notifications.py
+++ jasmin_manage/notifications.py
@@ -116,25 +116,25 @@ def notify_slack_project_submitted_for_provisioning(event):
                         + "' consortium.",
                     },
                 },
-                {
-                    "type": "section",
-                    "fields": [
-                        {
-                            "type": "mrkdwn",
-                            "text": ">*Comment:*\n>*"
-                            + comments[0].created_at.strftime("%d %b %y %H:%M")
-                            + "* ' _"
-                            + comments[0].content
-                            + "_ '",
-                        }
-                    ],
-                },
-                {
-                    "type": "section",
-                    "fields": [
-                        {"type": "mrkdwn", "text": service_str},
-                    ],
-                },
+                # {
+                #     "type": "section",
+                #     "fields": [
+                #         {
+                #             "type": "mrkdwn",
+                #             "text": ">*Comment:*\n>*"
+                #             + comments[0].created_at.strftime("%d %b %y %H:%M")
+                #             + "* ' _"
+                #             + comments[0].content
+                #             + "_ '",
+                #         }
+                #     ],
+                # },
+                # {
+                #     "type": "section",
+                #     "fields": [
+                #         {"type": "mrkdwn", "text": service_str},
+                #     ],
+                # },
             ],
         }
         print(json.dumps(message))
