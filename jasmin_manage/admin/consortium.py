556005c899f0aa012fda2f9714127b3104a976d6
Matthew Jones <matthew.j.jones@stfc.ac.uk>
2024-06-11 11:46:32 +0100

    remove all extra columns

--- jasmin_manage/admin/consortium.py
+++ jasmin_manage/admin/consortium.py
@@ -24,63 +24,63 @@ class ConsortiumAdmin(admin.ModelAdmin):
         css = {"all": ("css/admin/highlight.css",)}
         js = ("js/admin/highlight.js",)
 
-    list_display = ("name", "is_public", "manager_link")
-    list_select_related = ("manager",)
+    list_display = ("name", "is_public")#, "manager_link")
+    #list_select_related = ("manager",)
     search_fields = ("name",)
-    autocomplete_fields = ("manager",)
+    #autocomplete_fields = ("manager",)
     
 
-    def get_exclude(self, request, obj=None):
-        exclude = tuple(super().get_exclude(request, obj) or ())
-        if obj and not self.has_change_permission(request, obj):
-            return exclude + ("manager",)
-        else:
-            return exclude
+    # def get_exclude(self, request, obj=None):
+    #     exclude = tuple(super().get_exclude(request, obj) or ())
+    #     if obj and not self.has_change_permission(request, obj):
+    #         return exclude + ("manager",)
+    #     else:
+    #         return exclude
 
-    def get_readonly_fields(self, request, obj=None):
-        readonly_fields = super().get_readonly_fields(request, obj)
-        if obj and not self.has_change_permission(request, obj):
-            return ("manager_link",) + readonly_fields
-        elif not obj:
-            return ()
-        else:
-            return readonly_fields
+    # def get_readonly_fields(self, request, obj=None):
+    #     readonly_fields = super().get_readonly_fields(request, obj)
+    #     if obj and not self.has_change_permission(request, obj):
+    #         return ("manager_link",) + readonly_fields
+    #     elif not obj:
+    #         return ()
+    #     else:
+    #         return readonly_fields
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        # Annotate the consortia with information about the number of quotas, projects and requirements
-        qs = qs.annotate(
-            quota_count=Count("quota", distinct=True),
-            project_count=Count("project", distinct=True),
-            requirement_count=Count("project__service__requirement", distinct=True),
-        )
-        # Also annotate with information about the number of overprovisioned quotas
-        overprovisioned_quotas = (
-            Quota.objects.filter(consortium=OuterRef("pk"))
-            .annotate_usage()
-            .annotate(
-                overprovisioned=Case(
-                    When(provisioned_total__gt=F("amount"), then=1),
-                    default=Value(0),
-                    output_field=IntegerField(),
-                )
-            )
-        )
-        qs = qs.annotate(
-            overprovisioned_count=Coalesce(
-                Subquery(
-                    overprovisioned_quotas.order_by()
-                    .values("consortium")
-                    .annotate(overprovisioned_count=Sum("overprovisioned"))
-                    .values("overprovisioned_count")
-                ),
-                Value(0),
-            )
-        )
-        # The annotations remove the ordering, so re-apply the default ones
-        return qs.order_by(*qs.query.get_meta().ordering)
+    # def get_queryset(self, request):
+    #     qs = super().get_queryset(request)
+    #     # Annotate the consortia with information about the number of quotas, projects and requirements
+    #     # qs = qs.annotate(
+    #     #     quota_count=Count("quota", distinct=True),
+    #     #     project_count=Count("project", distinct=True),
+    #     #     requirement_count=Count("project__service__requirement", distinct=True),
+    #     # )
+    #     # # Also annotate with information about the number of overprovisioned quotas
+    #     # overprovisioned_quotas = (
+    #     #     Quota.objects.filter(consortium=OuterRef("pk"))
+    #     #     .annotate_usage()
+    #     #     .annotate(
+    #     #         overprovisioned=Case(
+    #     #             When(provisioned_total__gt=F("amount"), then=1),
+    #     #             default=Value(0),
+    #     #             output_field=IntegerField(),
+    #     #         )
+    #     #     )
+    #     # )
+    #     # qs = qs.annotate(
+    #     #     overprovisioned_count=Coalesce(
+    #     #         Subquery(
+    #     #             overprovisioned_quotas.order_by()
+    #     #             .values("consortium")
+    #     #             .annotate(overprovisioned_count=Sum("overprovisioned"))
+    #     #             .values("overprovisioned_count")
+    #     #         ),
+    #     #         Value(0),
+    #     #     )
+    #     # )
+    #     # The annotations remove the ordering, so re-apply the default ones
+    #     return qs.order_by(*qs.query.get_meta().ordering)
 
-    def manager_link(self, obj):
-        return change_link(obj.manager)
+    # def manager_link(self, obj):
+    #     return change_link(obj.manager)
 
-    manager_link.short_description = "manager"
+    # manager_link.short_description = "manager"
