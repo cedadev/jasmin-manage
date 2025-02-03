171ae9195154c9c8b0beaef14f96276efaaf66d1
Matthew Jones <matthew.j.jones@stfc.ac.uk>
2025-02-03 09:58:01 +0000

    Revert consortium test view changes

--- jasmin_manage/tests/views/test_consortium.py
+++ jasmin_manage/tests/views/test_consortium.py
@@ -383,23 +383,21 @@ class ConsortiumQuotasViewSetTestCase(TestCase):
 
     def test_list_non_public_staff_user_not_manager(self):
         """
-        Tests that the list endpoint returns okay for a non-public consortium and a staff
+        Tests that the list endpoint returns forbidden for a non-public consortium and a staff
         user who is not the consortium manager.
+
+        This should be forbidden rather than not found because the user can see the consortium.
         """
         user = self.authenticate()
         user.is_staff = True
         user.save()
         consortium = Consortium.objects.filter(is_public=False).order_by("?").first()
-        self.assertListResponseMatchesQuerySet(
-            "/consortia/{}/quotas/".format(consortium.pk),
-            consortium.quotas.annotate_usage(),
-            QuotaSerializer,
-        )
+        self.assertPermissionDenied("/consortia/{}/quotas/".format(consortium.pk))
 
     def test_list_non_public_user_belongs_to_project_not_manager(self):
         """
         Tests that the endpoint for a non-public consortium where the user owns a project
-        in the consortium return the list successfully.
+        in the consortium return the list successfully
         """
         user = self.authenticate()
         # Pick a non-public consortium
